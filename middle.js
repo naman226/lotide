f39e8eb090577cdcbba01290a7477df166e27d10
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-01 17:06:24 +0000

    Fixed according to spacing benchmark

--- middle.js
+++ middle.js
@@ -19,19 +19,19 @@ let assertArraysEqual = function(array1, array2) {
 };
 
 let middle = function(array) {
-let array1 = [];
-if (array.length <= 2) {
-  return array1;
-} else if ((array.length % 2) !== 0) {
-  let i = Math.floor(array.length / 2);
-  array1.push(array[i]);
-  return array1;
-} else if ((array.length % 2) === 0) {
-  let j = array.length / 2;
-  let k = j - 1;
-  array1.push(array[k], array[j]);
-  return array1;
-}
+  let array1 = [];
+  if (array.length <= 2) {
+    return array1;
+  } else if ((array.length % 2) !== 0) {
+    let i = Math.floor(array.length / 2);
+    array1.push(array[i]);
+    return array1;
+  } else if ((array.length % 2) === 0) {
+    let j = array.length / 2;
+    let k = j - 1;
+    array1.push(array[k], array[j]);
+    return array1;
+  }
 };
 assertArraysEqual((middle([1,2,3,4])), [2,3]);
 
