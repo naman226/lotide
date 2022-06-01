e810dc83fe776f5caddd0d1050bbe6d3a5d9f164
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-01 23:12:41 +0000

    Counts letters in a string

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let countLetters = function(strings) {
+  let finalCount = {};
+  for (let string of strings) {
+    if (string !== ' ') {
+      if (finalCount[string]) {
+        finalCount[string] += 1;
+      } else {
+        finalCount[string] = 1;
+      }
+    }
+  }
+  return finalCount;
+};
+console.log(countLetters("lighthouse in the house"));
+assertEqual(countLetters("lightlighter").l, 2);
