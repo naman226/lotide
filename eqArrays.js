9063e80647175b153ce3a9b9d4b0320366c31984
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-01 01:50:32 +0000

    function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+  
+let eqArrays = function(array1 , array2) {
+    if (array1.length !== array2.length) {
+      return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); 
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); 
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); 
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+
