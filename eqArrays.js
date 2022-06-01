1a1697f041646eee8f0e287c148565d06a61b5cd
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-01 01:53:59 +0000

    fixed spacing

--- eqArrays.js
+++ eqArrays.js
@@ -9,22 +9,22 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
   
 let eqArrays = function(array1 , array2) {
-    if (array1.length !== array2.length) {
-      return false;
+  if (array1.length !== array2.length) {
+    return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); 
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); 
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); 
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
 
 
