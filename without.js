547ef8989794e2735c0b036ed79a6a3a735ef04b
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-01 03:46:02 +0000

    Eliminates items to be moved from source array

---
+++ without.js
@@ -0,0 +1,35 @@
+let eqArrays = function(array1 , array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("✅✅ Arrays match");
+  } else {
+    console.log("🛑🛑 Arrays do not match");
+  }
+};
+
+let without = function(source, eliminator) {
+ let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+  if (!eliminator.includes(source[i])) {
+    newArray.push(source[i]);
+ }
+}
+return newArray;
+};
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"])); 
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
