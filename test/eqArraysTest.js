a8fa94f406620af1048be411dc35d1e225ef3773
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-06 20:18:41 +0000

    eqArray test by module export

---
+++ test/eqArraysTest.js
@@ -0,0 +1,9 @@
+let assertEqual = require('../assertEqual');
+let eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 5]), false);
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
