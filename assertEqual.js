b579a87709d91dcab1ef656dfe8fe57b8a5ad9a1
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-06 16:44:44 +0000

    Restructure using module

--- assertEqual.js
+++ assertEqual.js
@@ -5,6 +5,5 @@ let assertEqual = function(actual, expected) {
     console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-  
+module.exports = assertEqual;
+
