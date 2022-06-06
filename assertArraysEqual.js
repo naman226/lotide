1d376868a4d041e0e9414e4178146ca9bdf87c26
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-06 23:40:34 +0000

    Restructure for testing

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,8 +3,10 @@ let eqArrays = require('./eqArrays');
 let assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log("✅✅ Arrays match");
+    return true;
   } else {
     console.log("🛑🛑 Arrays do not match");
+    return false;
   }
 };
 module.exports = assertArraysEqual;
