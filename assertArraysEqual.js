614a254485223d90c2a63ad3f0ce14a2354fa48e
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-06 20:41:25 +0000

    Restructure with module export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-  
-let eqArrays = function(array1 , array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+let eqArrays = require('./eqArrays');
 
 let assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -18,4 +7,4 @@ let assertArraysEqual = function(array1, array2) {
     console.log("🛑🛑 Arrays do not match");
   }
 };
-assertArraysEqual([1,2,3], [1,2,3]);
+module.exports = assertArraysEqual;
