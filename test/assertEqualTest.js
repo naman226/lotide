775aa92d8f6a5be82b3b8fdd70361ebf10a395ec
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-06 23:08:30 +0000

    Testing with Mocha and Chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,12 @@
-let assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
+const assertEqual = require('../assertEqual.js');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-  
+describe("#assertEqual", () => {
+  it("returns false when values are not equal", () => {
+    assert.isFalse(assertEqual("Lighthouse Labs", "Bootcamp"));
+  });
+
+  it("returns true when values are equal", () => {
+    assert.isTrue(assertEqual(1, 1));
+  });
+});
