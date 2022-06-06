9b3cc03289999fbc9bca03d8b0e9cc90f1e875c9
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-06 23:09:22 +0000

    Restructure for testing

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,10 @@
-let assertEqual = function(actual, expected) {
+let assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
   } else {
     console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return false;
   }
 };
 module.exports = assertEqual;
