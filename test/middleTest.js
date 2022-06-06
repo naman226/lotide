2cbd18543a7ff280d4a72c15264de06ffde37aa1
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-06 22:13:30 +0000

    Removed commented statements

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,3 @@
-// let middle = require('../middle');
-// let assertArraysEqual = require('../assertArraysEqual');
-
-// assertArraysEqual((middle([1,2,3,4])), [2,3]);
-// assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']);
-
 const assert = require('chai').assert;
 const middle = require('../middle');
 
